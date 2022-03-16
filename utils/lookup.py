c6921631a36349aa9deb0ebb469521dc4fa8d3a7
yuezih <1103284182@qq.com>
2022-03-16 12:33:28 +0800

    LookupTable (improved table)

--- utils/lookup.py
+++ utils/lookup.py
@@ -14,16 +14,18 @@ def get_lookup_list(i):
     lookup_list = []
     for attr in anno[i]['attr']:
         lookup_list += attr_dict[attr]
-    # 保留lookup_list中出现频率最高的5个值
-    lookup_list = sorted(lookup_list, key=lookup_list.count, reverse=True)
-    lookup_list = lookup_list[:5]
+    lookup_list = sorted(set(lookup_list), key=lookup_list.count, reverse=True)
+    # 将anno[i]['images'][0]移出lookup_list
+    if anno[i]['images'][0] in lookup_list:
+        lookup_list.remove(anno[i]['images'][0])
+    lookup_list = [anno[i]['images'][0]] + lookup_list[:4]
     print(i, lookup_list)
     id_dict[i] = lookup_list
 
 def main():
     with ProcessPoolExecutor() as executor:
         executor.map(get_lookup_list, range(len(anno)))
-    with open('../../Dataset/Fashion-MMT/Clean/anno_clean_lookup.json', 'w') as f:
+    with open('../../Dataset/Fashion-MMT/Clean/anno_clean_lookup_pro.json', 'w') as f:
         for i in range(len(anno)):
             anno[i]['lookup'] = id_dict[i]
             print(f'{i} finished')
